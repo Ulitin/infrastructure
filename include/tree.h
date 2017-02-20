e77174406cf91cb7413dbc050f51319b272b75b5
Улитин Александр Владимирович <ulitin.a@ccam.cmc.unn.net>
2017-02-20 18:31:24 +0300

    add function find and add element in tree

---
+++ include/tree.h
@@ -0,0 +1,37 @@
+#ifndef INCLUDE_TREE_H_
+#define INCLUDE_TREE_H_
+#include "stdio.h"
+
+struct CNode {
+  int inf;
+  CNode* left;
+  CNode* right;
+
+ public:
+  explicit CNode(const int v = 0) {
+    this->left = 0;
+    this->right = 0;
+    this->inf = v;
+  }
+
+  CNode(const CNode &root) {
+    if (this != &root) {
+      this->inf = root.inf;
+      while (root.right) {
+        this->right = new CNode;
+        this->right = root.right;
+      }
+      while (root.left) {
+        this->left = new CNode;
+        this->left = root.left;
+      }
+    }
+  }
+
+  // void print_tree();
+  // void input_tree();
+};
+
+  CNode* find_or_add_el(CNode *root, int v);
+
+#endif  // INCLUDE_TREE_H_
--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,59 @@
 #include <gtest/gtest.h>
-#include "add.h"
+#include "tree.h"
 
-TEST(Addition, CanAddTwoNumbers) {
-  EXPECT_EQ(add(2, 2), 4);
-  EXPECT_EQ(add(-2, 2), 0);
+TEST(Addition, Test_on_correct_store) {
+  CNode tree(8);
+  tree.right = new CNode(15);
+  tree.right->right = new CNode(20);
+  tree.right->left = new CNode(10);
+  tree.left = new CNode(4);
+  tree.left->right = new CNode(7);
+  tree.left->left = new CNode(2);
+  EXPECT_EQ(tree.inf, 8);
+  EXPECT_EQ(tree.right->inf, 15);
+  EXPECT_EQ(tree.right->right->inf, 20);
+  EXPECT_EQ(tree.right->left->inf, 10);
+  EXPECT_EQ(tree.left->inf, 4);
+  EXPECT_EQ(tree.left->right->inf, 7);
+  EXPECT_EQ(tree.left->left->inf, 2);
+}
+
+TEST(Addition, Test_on_find) {
+  CNode* tree = new CNode(8);
+  CNode* find_el;
+  tree->right = new CNode(15);
+  tree->right->right = new CNode(20);
+  tree->right->left = new CNode(10);
+  tree->left = new CNode(4);
+  tree->left->right = new CNode(7);
+  tree->left->left = new CNode(2);
+  find_el = find_or_add_el(tree, 7);
+  EXPECT_EQ(tree->inf, 8);
+  EXPECT_EQ(tree->right->inf, 15);
+  EXPECT_EQ(tree->right->right->inf, 20);
+  EXPECT_EQ(tree->right->left->inf, 10);
+  EXPECT_EQ(tree->left->inf, 4);
+  EXPECT_EQ(tree->left->right->inf, 7);
+  EXPECT_EQ(tree->left->left->inf, 2);
+  EXPECT_EQ(7, find_el->inf);
+}
+
+TEST(Addition, Test_on_add) {
+  CNode* tree = new CNode(8);
+  CNode* find_el;
+  tree->right = new CNode(15);
+  tree->right->right = new CNode(20);
+  tree->right->left = new CNode(10);
+  tree->left = new CNode(4);
+  tree->left->left = new CNode(2);
+  find_el = find_or_add_el(tree, 7);
+  EXPECT_EQ(tree->inf, 8);
+  EXPECT_EQ(tree->right->inf, 15);
+  EXPECT_EQ(tree->right->right->inf, 20);
+  EXPECT_EQ(tree->right->left->inf, 10);
+  EXPECT_EQ(tree->left->inf, 4);
+  EXPECT_EQ(tree->left->right->inf, 7);
+  EXPECT_EQ(tree->left->left->inf, 2);
 }
 
 int main(int ac, char* av[]) {
