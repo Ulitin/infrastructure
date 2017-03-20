9f8644df6430a963ce223166f9d8e0588bb9e00e
Ulitin <sashavladimirovichulitin@gmail.com>
2017-03-20 19:21:12 +0300

    correct cod

--- include/task.h
+++ include/task.h
@@ -1,6 +1,6 @@
-#ifndef INCLUDE_ADD_H_
-#define INCLUDE_ADD_H_
+#ifndef INCLUDE_TASK_H_
+#define INCLUDE_TASK_H_
 
 int* fun(int *mas, int size);
 
-#endif  // INCLUDE_ADD_H_
+#endif  // INCLUDE_TASK_H_
